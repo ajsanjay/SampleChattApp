744081068503ff09e4bafccc4d331311a9482017
Jaya Sabeen <jayasabeen@Jayas-MacBook-Pro.local>
2025-04-23 14:47:33 +0530

    first commit

---
+++ SampleChattApp/SampleChattAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleChattAppApp.swift
+//  SampleChattApp
+//
+//  Created by Jaya Sabeen on 23/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SampleChattAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
